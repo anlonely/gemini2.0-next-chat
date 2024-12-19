27a2f4383c20073bc047edf625507c35fd805c26
anlonely <35134626+anlonely@users.noreply.github.com>
2024-12-19 17:06:12 +0800

    Update model.ts

--- constant/model.ts
+++ constant/model.ts
@@ -1,18 +1,7 @@
 export const Model: Record<string, string> = {
-  'gemini-1.5-pro': 'Gemini 1.5 Pro',
-  'gemini-1.5-pro-latest': 'Gemini 1.5 Pro Latest',
-  'gemini-1.5-flash': 'Gemini 1.5 Flash',
-  'gemini-1.5-flash-latest': 'Gemini 1.5 Flash Latest',
-  'gemini-1.5-flash-8b': 'Gemini 1.5 Flash-8B',
-  'gemini-1.5-flash-8b-latest': 'Gemini 1.5 Flash-8B Latest',
-  'gemini-1.0-pro-vision': 'Gemini 1.0 Pro Vision',
-  'gemini-1.0-pro-vision-latest': 'Gemini 1.0 Pro Vision Latest',
-  'gemini-1.0-pro': 'Gemini 1.0 Pro',
-  'gemini-1.0-pro-latest': 'Gemini 1.0 Pro Latest',
-  'gemini-pro-vision': 'Gemini Pro Vision',
-  'gemini-pro': 'Gemini Pro',
+  'gemini-2.0-flash-exp': 'gemini-2.0-flash-exp',
 }
 
-export const OldVisionModel = ['gemini-pro-vision', 'gemini-1.0-pro-vision-latest']
+export const OldVisionModel = ['gemini-2.0-flash-expn', 'gemini-2.0-flash-exp']
 
-export const OldTextModel = ['gemini-1.0-pro', 'gemini-1.0-pro-latest', 'gemini-pro']
+export const OldTextModel = ['gemini-2.0-flash-exp', 'gemini-2.0-flash-exp', 'gemini-2.0-flash-exp']
